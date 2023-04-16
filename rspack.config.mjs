9f68da6c3609eb904e65eba918998ca93751ee45
YunfeiHe <i.heyunfei@gmail.com>
2023-04-16 20:51:15 +0800

    build: support build

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,7 +1,18 @@
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
+
+if (!isProd && !isDev) {
+  throw new Error(`Unknown env ${process.env.NODE_ENV}`)
+}
+
 /**
  * @type {import('@rspack/cli').Configuration}
  */
 export default {
+  mode: isProd ? 'production' : 'none',
+  output: {
+    filename: '[name]-[hash:8][ext]',
+  },
   entry: './src/main',
   module: {
     rules: [
